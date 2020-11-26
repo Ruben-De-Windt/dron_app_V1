ce7d1d679b8ae94ab9031d5c38afa1420d16f005
Ruben De Windt <r0780268@student.vives.be>
2020-11-26 20:38:00 +0100

    programatie van een camera

--- ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MApplication.java
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MApplication.java
@@ -6,11 +6,21 @@ import android.content.Context;
 import com.secneo.sdk.Helper;
 
 public class MApplication extends Application {
-
+    private CAM cam;
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
+        if (cam == null) {
+            cam = new CAM();
+            cam.setContext(this);
+        }
+
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        cam.onCreate();
     }
 
 }
